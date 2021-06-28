fecc8254e6e40ed7702dee0a64f6dfbc6d4754d3
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2021-06-28 12:54:23 +0530

    Test Widget

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box, Heading } from "@chakra-ui/react";
 
-function App() {
+function App({ container }) {
+  const message = container.getAttribute("data-message");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box boxShadow="base">
+      <Heading>
+        {message}
+      </Heading>
+    </Box>
   );
 }
 
